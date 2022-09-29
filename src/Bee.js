4cc1f89b76c52cdc7b9bf9014d238ac2addf28ff
CJ Edgecombe <cjledgecombe@gmail.com>
2022-09-29 17:59:20 -0600

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -4,6 +4,6 @@ var Bee = function() {
 	this.color = 'yellow';
 	this.job = 'keep on growing';
 };
-
+// empty commit
 Bee.prototype = Object.create(Grub.prototype);
 Bee.prototype.constructor = Bee;
